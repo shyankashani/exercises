4ca3c20c9d04bdf43a4e589a8d40ee1180e0b4ab
Shyan Kashani <shyankashani1@gmail.com>
2017-08-08 19:20:52 -0700

    Completed waterBlocks with time complex solution

--- waterBlocks.js
+++ waterBlocks.js
@@ -1,39 +1,51 @@
-var waterBlocks = function (blocks) {
+var waterBlocks = function(blocks) {
   var water = 0;
 
-  var recurse = function (towers) {
-    if (!checkWater(towers)) {
-      return water;
-    } else if (towers.contains(0)) {
-      var waterSoFar = 0;
-      for (var i = 0; i < towers.length; i++) {
-        if (towers[i] === 0) {
-          waterSoFar++;
-        } else {
+  var recurse = function(blocks) {
+    // Declare a waterSoFar variable and set it equal to zero
+    var waterSoFar = 0;
+
+    // Declare a towerBehind variable and set it equal to false
+    var towerBehind = false;
+
+    // If the sum of all the numbers in the array of blocks is equal to 0
+    if (blocks.reduce((a,b) => { return a + b }, 0) === 0) {
+      // Return
+      return;
+      // Else
+    } else {
+      // Loop through the blocks
+      for (var i = 0; i < blocks.length; i++) {
+        // If the block is greater than 0
+        if (blocks[i] > 0 && towerBehind === true) {
+          // Add waterSoFar to water
           water = water + waterSoFar;
+          // Set waterSoFar to zero
           waterSoFar = 0;
+          // If the block is equal to 0
+        } else if (blocks[i] > 0 && towerBehind === false) {
+          towerBehind = true;
+        } else if (blocks[i] === 0 && towerBehind === true) {
+          // Add 1 to waterSoFar
+          waterSoFar++;
         }
       }
-    } else {
-      normalize(towers);
-      recurse(towers);
+
+      // Loop through the blocks
+      for (var j = 0; j < blocks.length; j++) {
+        // If the block is greater than 0
+        if (blocks[j] > 0) {
+          // Subtract one from the block
+          blocks[j] = blocks[j] - 1;
+        }
+      }
+      // Perform the recursive function on the newly normalize array of blocks
+      recurse(blocks);
     }
+
   };
 
   recurse(blocks);
 
-};
-
-
-
-var normalize = function (blocks) {
-  for (var i = 0; i < blocks.length; i++) {
-    if (blocks[i] > 0) { blocks[i] = blocks[i] - 1 }
-  }
-};
-
-var checkWater = function (blocks) {
-  return blocks.reduce((a,b) => {
-    return a + b;
-  })
+  return water;
 }
