d18ea220642ae0cda0345cb1b466263f100edb88
Shyan Kashani <shyankashani1@gmail.com>
2017-08-10 19:09:53 -0700

    Completed binarySearchTree's breadthFirstSearch

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,26 +1,65 @@
 const breadthFirstSearch = (tree, callback) => {
   // Create a queue variable that is an array
-  let queue = [];
+  let queue = [tree];
   // Create a variable n and set it equal to 1
-  let n = 1;
-
+  let n = queue.length;
   // Loop through the length of n
   while (n) {
-    // Push the tree into the queue
-    queue.push(tree);
     // If the zeroeth tree in the queue has left, push that into the queue
     if (queue[0].left) { queue.push(queue[0].left) }
     // If the zeroeth tree in the queue has right, push that into the queue
     if (queue[0].right) { queue.push(queue[0].right) }
     // Call the callback on that tree
-    callback(queue[0]);
-    // Unshift the first item from the queue and set n equal to the length of the queue
-    n = queue.unshift()
+    callback(queue.shift());
+    // Set n equal to the queue's length
+    n = queue.length;
   }
 }
 
 const depthFirstSearch = (tree, callback) => {
-    const recurse = tree => {
+  // Create a stack variable that is an array with the tree inside of it
 
-    };
+  // Create a variable n and set it equal to the lenght of the stack
+
+  // While n
+
+  // If the last tree in the stack has a right, push that into the stack
+
+  // If the last tree in the stack has a left, push that into the stack
+
+  // Pop the first
 }
+
+const tree = {
+  value: 1,
+  left: {
+    value: 2,
+    left: {
+      value: 3,
+      left: undefined,
+      right: undefined
+    },
+    right: {
+      value: 4,
+      left: undefined,
+      right: undefined
+    }
+  },
+  right: {
+    value: 5,
+    left: {
+      value: 6,
+      left: undefined,
+      right: undefined
+    },
+    right: {
+      value: 7,
+      left: undefined,
+      right: undefined
+    }
+  }
+}
+
+
+
+breadthFirstSearch(tree, (node) => { console.log(node.value) });
