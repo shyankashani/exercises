89e2c7fe657436c98d2991a01f064efc2d2ae170
Shyan Kashani <shyankashani1@gmail.com>
2017-08-27 11:31:56 -0700

    Pseudocoded addTwoNumbers

---
+++ addTwoNumbers.js
@@ -0,0 +1,29 @@
+var addTwoNumbers = (l1, l2) => {
+
+  // Create a recursive function that accepts a node and a number so far array
+    // If the node has a value
+      // Unshift the number into the number so far array
+    // If the node has a next
+      // Perform the recursive function on the next node and the new number so far array
+    // Otherwise
+      // Return the concatenation and toNumberization of the new number so far array
+
+  // Declare a variable called n1 and set it equal to the recursive function performed on l1 node and an empty array
+
+  // Declare a variable called n2 and set it equal to the recursive function performed on l2 node and an empty array
+
+  // Create a recursive insert function that accepts a node and a number
+    // If the node's next is null
+      // Set the node's next equal to a new node passing in the number
+    // Else
+      // Perform the recursive insert function on the node's next
+
+  // Declare a variable called outputList
+
+  // Create a recursive function that accepts a number
+    // Convert the number into a string
+    // Iterate through the string backward
+      // Insert the toNumberization of the string into the outputList
+
+  // Return the outputList
+}
