c041c9efe14575478e4b3b35a3980bd05291e639
Shyan Kashani <shyankashani1@gmail.com>
2017-08-30 10:59:35 -0700

    Completed plusOne

--- plusOne.js
+++ plusOne.js
@@ -1,18 +1,16 @@
 var plusOne = function(digits) {
-  // create a for loop that moves backward through the digits array
   for (var i = digits.length - 1; i > -1; i--) {
-    // add one to the digit
     digits[i]++;
-    // if the digit is ten
     if (digits[i] === 10) {
-      // turn the digit into zero
       digits[i] = 0;
-      // continue
-      continue;
-    // otherwise
-  } else {
-    // return the digits array
-    break;
+      if (i === 0) {
+        digits.unshift(1)
+        break;
+      } else {
+        continue;
+      }
+    } else {
+      break;
     }
   }
   return digits;
