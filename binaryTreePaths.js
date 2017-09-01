a3fffc0f6b12aa7e865e8b7bbe508163006f160d
Shyan Kashani <shyankashani1@gmail.com>
2017-09-01 13:36:21 -0700

    Psuedocoded binaryTreePaths

---
+++ binaryTreePaths.js
@@ -0,0 +1,13 @@
+var binaryTreePaths = function(root) {
+  // create a results array
+
+  // create a recursive function that accepts a tree and a path so far
+    // if the tree has a left
+      // call the recursive function, passing in left and the path so far string with the new tree appended to it
+    // if the tree has a right
+      // call the recursive function, passing in right and the path so far string with the new tree appended to it
+    // if the tree has neither a left nor right
+      // push path so far into the results array
+
+  // perform the recursive, passing in the root and the root's value in a string form
+};
