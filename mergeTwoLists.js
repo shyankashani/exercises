c7a64ad74a0a1afa5fddb5237b3e14a39053aa0c
Shyan Kashani <shyankashani1@gmail.com>
2017-09-04 10:02:16 -0700

    Pseudocoded mergeTwoLists

---
+++ mergeTwoLists.js
@@ -0,0 +1,26 @@
+var mergeTwoLsts = function(l1, l2) {
+
+    // create a variable called the initial node
+
+    // create a recursive function that accepts two nodes and a current node
+      // if the first node and the second node are equal to null
+        // return
+      // if the value of the first node is less than the value of the second node
+      // or if the second node is null
+        // make the first node the next of the current node
+        // perform the recursive function on the the next of the first node, the second node, and the next of the current node
+      // if the value of the first node is greater than the value of the second node
+      // or if the first node is null
+        // make the second node the next of the current node
+        // perform the recursive function on the first node, the next of the second node, and the next of the current node
+
+    // if the value of the first node is less than the value of the second node
+      // set initial node equal to the first node
+      // perform the recursive function on the next of the first node, the second node, and the initial node
+    // otherwise
+      // set the initial node equal to the second node
+        // perform the recursive function on the the first node, the next of the second node, and the initial node
+
+    // return the initial node
+
+};
