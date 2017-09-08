7312023f00147cb30496f7a2f634e8061f7e505f
Shyan Kashani <shyankashani1@gmail.com>
2017-09-07 17:22:32 -0700

    Completed searchInsert

--- searchInsert.js
+++ searchInsert.js
@@ -1,7 +1,15 @@
 var searchInsert = function(nums, target) {
   // Iterate through the nums array
+  for (var i = 0; i < nums.length; i++) {
     // If the num is equal to the target or if the num is greater than the target
+    if (target <= nums[i]) {
       // Return the index of the num
+      return i;
+    }
     // If the num is the last num in the nums array
-      // Return the length of hte nums array
+    if (i === nums.length - 1) {
+      // Return the length of the nums array
+      return nums.length;
+    }
+  }
 };
