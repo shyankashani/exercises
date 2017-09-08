434f81b070a13d8cd0dbe5796f534d6f88986b1b
Shyan Kashani <shyankashani1@gmail.com>
2017-09-07 17:18:50 -0700

    Pseudocoded searchInsert

---
+++ searchInsert.js
@@ -0,0 +1,7 @@
+var searchInsert = function(nums, target) {
+  // Iterate through the nums array
+    // If the num is equal to the target or if the num is greater than the target
+      // Return the index of the num
+    // If the num is the last num in the nums array
+      // Return the length of hte nums array
+};
