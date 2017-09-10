c59fc1a893ee7c711b19b8e8bef8f25296ae7e46
Shyan Kashani <shyankashani1@gmail.com>
2017-09-10 12:36:55 -0700

    Completed quickSort with swapping

--- quickSort.js
+++ quickSort.js
@@ -18,3 +18,38 @@ var quickSort = array => {
   }
   return quickSort(left).concat([pivot]).concat(quickSort(right));
 }
+
+
+// create a quicksort function that accepts an array
+var quickSortSwapping = array => {
+
+  if (array.length <= 1) { return array; }
+
+  // set the pivot equal to the popped off element of the array
+  var pivot = array.pop();
+
+  // set variable i equal to -1
+  var i = -1;
+
+  // iterate through the length of the array using j beginning at 1
+  for (var j = 0; j < array.length; j++) {
+    // if the item at j is less than the pivot
+    if (array[j] < pivot) {
+      // increment i
+      i++;
+      // swap the element at i with the element at j
+      console.log(array);
+      swap(array, i, j);
+      console.log(array);
+    }
+  }
+
+  // perform quickSort on the array up until and including i, concat the pivot there, and then after the pivot
+  return quickSort(array.slice(0, i + 1)).concat([pivot]).concat(quickSort(array.slice(i + 1)));
+}
+
+var swap = (array, m, n) => {
+  var temp = array[n];
+  array[n] = array[m];
+  array[m] = temp;
+}
