71a95d23e4b9a7fa33cef0d7166d2c109d283ced
Shyan Kashani <shyankashani1@gmail.com>
2017-09-13 11:07:24 -0700

    Completed powerFilter

---
+++ powerFilter.js
@@ -0,0 +1,46 @@
+// Given an array, output the values that are a power of 2.
+
+// I: An array of numbers.
+// O: An array of numbers.
+// C: None, for now.
+// E: Empty array, all values are power of two, no values are power of two.
+
+var powerFilter = array => {
+  // create a results array
+  var results = [1];
+
+  // iterate through the array
+  for (var i = 0; i < array.length; i++) {
+    var original = array[i];
+    var modified = array[i];
+
+    // create a loop variable and set it equal to true
+    var loop = true;
+    // as long as the loop variable is true
+    while (loop) {
+      // if dividing the number by two yields a remainder
+      if (modified % 2 !== 0) {
+        // set the loop variable to false
+        loop = false;
+      // otherwise if dividing the number by two yields two
+      } else if (modified / 2 === 2 || modified / 2 === 1) {
+        // push the number into the results array
+        results.push(original);
+        // set the loop variable to false
+        loop = false;
+      // otherwise divide the number by two
+      } else {
+        modified = modified / 2;
+      }
+    }
+  }
+
+  // return the results array
+  return results;
+};
+
+
+var a = [1,2,3,4,5,6,7,8,9,10]; // -> [1,2,4,8]
+
+
+console.log(powerFilter(a));
