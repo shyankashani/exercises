41c75d332f0d79494c5a019195a04ce08262b1da
Shyan Kashani <shyankashani1@gmail.com>
2017-09-18 19:45:01 -0700

    Some progress on maxSubArray

---
+++ leetcode/maxSubArray.js
@@ -0,0 +1,19 @@
+// @param {number[]} nums
+// @return {number}
+
+var maxSubArray = function(nums) {
+  // create a largest sum
+  var sum = 0;
+
+  // iterate over the array
+  for (var i = 0; i < nums.length; i++) {
+    // iterate over the array
+    for (var j = i + 1; j)
+      // if the number plus the largest sum is greater than the largest sum
+        // set it equal to the largest sum
+      // otherwise
+        // break
+  }
+
+  // return a largest sum
+};
