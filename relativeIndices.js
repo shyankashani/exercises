05943664273e09174642e95c133a86d23dc079fe
Shyan Kashani <shyankashani1@gmail.com>
2017-09-21 18:25:15 -0700

    Some progress on relativeIndices

---
+++ relativeIndices.js
@@ -0,0 +1,31 @@
+let set = new Set()
+set.add(3)
+set.add(5)
+
+{3, 5}
+
+console.log(set.has(20))
+
+
+
+{2,5,6}
+
+// [1,2,-2,1,-3]
+
+const isCircular = array => {
+
+  // set the pointer equal to the value at the zeroeth index of the array
+
+  // create a set for all the visited indices and initialize it with the zeroeth index
+
+  // loop
+    // if the pointer is equal to zero and the length of the set is equal to the length of the array
+      // return true
+    // if the pointer is equal to zero and the length of the set is not equal to the length of the array
+      // return false
+    // if the index exists in the set of visited indices
+      // return false
+    // add the pointer to the set of visited indices
+    // set the pointer equal to the pointer plus
+
+}
